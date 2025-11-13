9796adba082d6dee8676896906d8062376c02756
RIDONE HOSSAIN MOON <168392332+RidOneMoon@users.noreply.github.com>
2025-11-13 19:14:59 +0800

    the useAxios wasnt working because i miss add the /json

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "http://localhost:5000",
-  headers: { "Content-Type": "application/json" }
+  headers: { "Content-Type": "application" }
 });
 
 export default API;
